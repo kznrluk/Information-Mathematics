c21468481eddc9118f8504d02ea9ab676d32829f
kznrluk <kznr.luk@gmail.com>
2018-06-17 20:59:47 +0900

    コメントの追加

--- Euclid.js
+++ Euclid.js
@@ -1,3 +1,12 @@
+/*
+ * Euclid.js
+ * ユークリッドの互除法を利用しGCDを求めます。
+ */
+
+/*
+ * GCD (int, int)
+ * 再帰関数を利用し、x % yが0になるまで自身にyとxとyの商のあまりを渡し続けます。
+ */
 const GCD = (x, y) => !(x % y) ? y : GCD(y, x % y);
 
 console.log(GCD(3355, 2379));
