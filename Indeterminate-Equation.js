441442c66b46bb0dacecd47e5bb198622e9381f4
kznrluk <kznr.luk@gmail.com>
2018-06-17 21:00:45 +0900

    拡張ユークリッドの互除法を実装

--- Indeterminate-Equation.js
+++ Indeterminate-Equation.js
@@ -1,61 +1,46 @@
-const GCD = (x, y) => !(x % y) ? y : GCD(y, x % y);
+/*
+ * Indeterminate-Equation.js
+ * 拡張ユークリッドの互除法を利用しax + by = GCD(a, b)の答えを求めます。
+ */
 
 /*
- * generateFormulaOfGCD (int, int)
- * 2つの数を受け取り、GCDを算出するまでの過程を2次元配列で返します。
+ * extGCD (int a, int b)
+ * この関数はextendedEuclidAlgorithm()の短縮版です。
  */
-const generateFormulaOfGCD = (x, y) => {
-    const histry = [];
-    while(1){
-        const result = [x, y, Math.floor(x / y), x % y];
-        x = result[1];
-        y = result[3];
-        histry.push(result);
-        if(y === 0) break;
+const extGCD = (a, b) => !b ? [1, 0, a] : (x => [x[1], x[0]-x[1]*Math.floor(a/b), x[2]])(extGCD(b, a%b));
+
+/*
+ * extendedEuclidAlgorithm (int a, int b)
+ * a, cのGCDを求めた後、特殊解を求めます。
+ */
+const extendedEuclidAlgorithm = (a, b) => {
+    if (b === 0) {
+        // (2) GCDが求まる
+        console.log(`GCD is ${a}`)
+        return [1, b, a];
+    } else {
+        console.log(`${a} = ${b} * ${Math.floor(a/b)} + ${a%b}`)
+        // (1) GCDが求まるまで再帰する
+        const result = extendedEuclidAlgorithm(b, a % b);
+        x = result[0];
+        y = result[1];
+        // (3) GCDを元に遡り解を求める
+        console.log(`${result[2]} = ${a} * ${y} + ${b} * ${x - y * Math.floor(a/b)}`)
+        return [y, x - y * Math.floor(a/b), result[2]];
     }
-    return histry.map((value) => [value[3], value[0], value[2], value[1]]);
 }
 
-/*
- * getSolutionOfEquation (int a, int b, int c)
- * ax + by = c の方程式の解を返します。存在しない場合はnullを返します。
- */
-const getSolutionOfEquation = (a, b, c) => {
-    // TODO ループ範囲を適切な形に修正
-    for(let i = 0; i <= c * a; i++) {
-        if((c - (i * a)) % b == 0) {
-            return [i, (c - (i * a)) / b];
-        }
-    }
-    return null;
-}
+extendedEuclidAlgorithm(12707, 12319)
+// => 
+// 12707 = 12319 * 1 + 388
+// 12319 = 388 * 31 + 291
+// 388 = 291 * 1 + 97
+// 291 = 97 * 3 + 0
+// GCD is 97
+// 97 = 291 * 0 + 97 * 1
+// 97 = 388 * 1 + 291 * -1
+// 97 = 12319 * -1 + 388 * 32
+// 97 = 12707 * 32 + 12319 * -33
 
-/*
- * getParticularSolution (Array)
- * generateFormulaOfGCD()の結果を拡張ユークリッド互除法で解き、1つの解を返します。
- */
-const getParticularSolution = (resultArray) => {
-    // 配列を逆転する
-    formulas = resultArray.reverse();
-    // 自明であるので一番始めを削除
-    formulas.shift();
-
-    let a, b, solutions = [0, 0];
-    formulas.forEach((formula, index, array) => {
-        if(index === 0){
-            a = formula[1];
-            b = formula[3];
-        } else if (index%2 === 1){
-            b = array[index][1];
-        } else {
-            a = array[index][1]
-        }
-        solutions = getSolutionOfEquation(a, b, array[0][0]);
-        if(!solutions) throw '解がありません。';
-        console.log(`${array[0][0]} = ${a} * ${solutions[0]} + ${b} * ${solutions[1]}`);
-    });
-    return solutions;
-}
-
-console.log(getParticularSolution(generateFormulaOfGCD(1428, 1105)));
-// => [24, 31]
+console.log(extGCD(12707, 12319))
+// => [ 32, -33, 97 ]
