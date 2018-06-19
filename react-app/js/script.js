1c699d397785203a4849963c05e7e556dbd7374f
kznrluk <kznr.luk@gmail.com>
2018-06-19 12:50:28 +0900

    Reactを追加

---
+++ react-app/js/script.js
@@ -0,0 +1,14 @@
+ReactDOM.render(
+    MainContainer,
+    document.getElementById('root')
+);
+
+class MainContainer extends React.component {
+    render() {
+        return (
+            <div>
+                <h1>Hakozaki</h1>
+            </div>
+        );
+    }
+}
